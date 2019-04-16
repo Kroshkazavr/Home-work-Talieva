cba0e4a85a0a7cd90146497cca47e3543df608d8
Talieva Elena <erulen@mail.ru>
2019-04-16 15:24:29 +0300

    Добавдена задача 4.1

---
+++ src/ru/talieva/lesson04/task01/FindMinMax.java
@@ -0,0 +1,25 @@
+package ru.talieva.lesson04.task01;
+
+import java.util.Scanner;
+
+/** Класс, который находит Минимальное из 2-х чисел*/
+public class FindMinMax {
+    private static int first; /*первое число */
+    private static int second; /*второе число */
+    private static int getMin (){ /* метод для поиска Минимального числа*/
+        if (first == second) System.out.println("Числа равны");
+        else if (first<second) return first;
+        else return second;
+        return 0;/// ????? Почему нуюно добавить return 0? Без него не реботает. ?????
+    }
+
+    public static void main (String [] args) {
+        Scanner in = new Scanner (System.in); /*инициализация сканера*/
+        System.out.println("Добро пожаловать в программу по поиску минильного из 2-х натуральных чисел!");
+        System.out.println("Введите первое число.");
+        first = in.nextInt(); /*ввод первого числа с клавиатуры */
+        System.out.println("Введите второе число.");
+        second = in.nextInt(); /*ввод второго числа с клавиатуры */
+        System.out.println("Минимальное число: " + getMin());
+    }
+}
